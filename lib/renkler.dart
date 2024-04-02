80feb2a394f309b20c3b6d1ae05b3748e2dab4c2
OzlemSevinc <ozlemsevincc@hotmail.com>
2024-04-02 18:23:00 +0300

    First commit

--- lib/renkler.dart
+++ lib/renkler.dart
@@ -1,7 +1,7 @@
 
 import 'dart:ui';
 
-var anaPembe = const Color(0xFFEDA9B9);
+var anaPembe = const Color(0xFFEDA988);
 var siyah = const Color(0xFF000000);
 var beyaz = const Color(0xFFFFFFFF);
 var gri = const Color(0xFFD9D9D9);
